46a4b6822bd7f9714580e2c55a88a9687a8ca471
Keerthy Vasan <keerthivasankannan76@gmail.com>
2026-07-31 22:32:45 +0530

    Update website content and layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import Login from "./page/login/login";
 import Home from "./page/home/home";
 import { Route, Routes } from "react-router-dom";
-import Chat from "./components/chat/chat";
-import AdminChat from "./page/adminchat";
-import ChatPage from "./page/chatpage/chatpage";
+// import Chat from "./components/chat/chat";
+// import AdminChat from "./page/adminchat";
+// import ChatPage from "./page/chatpage/chatpage";
 
 function App() {
   return (
@@ -12,9 +12,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/chat" element={<Chat />} />
+        {/* <Route path="/chat" element={<Chat />} />
         <Route path="/adminchat" element={<AdminChat />} />
-        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/chat" element={<ChatPage />} /> */}
       </Routes>
     </>
   );
