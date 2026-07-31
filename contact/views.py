7bee3e747634877dd571ccc8387d0ec4cadc57b8
Keerthy Vasan <keerthivasankannan76@gmail.com>
2026-07-31 12:19:59 +0530

    Update views.py

--- contact/views.py
+++ contact/views.py
@@ -4,17 +4,16 @@ from .serializers import ContactSerializers
 from django.core.mail import send_mail
 from django.conf import settings
 
-
-@api_view(['post'])
+@api_view(['POST'])
 def contact_api(request):
-    print(request.data)
     serializer = ContactSerializers(data=request.data)
 
     if serializer.is_valid():
         serializer.save()
+
         send_mail(
-    subject="New Contact Form Message",
-    message=f"""
+            subject="New Contact Form Message",
+            message=f"""
 Name: {serializer.validated_data['first_name']} {serializer.validated_data['last_name']}
 
 Email: {serializer.validated_data['email']}
@@ -22,13 +21,14 @@ Email: {serializer.validated_data['email']}
 Message:
 {serializer.validated_data['message']}
 """,
-    from_email=settings.EMAIL_HOST_USER,
-    recipient_list=[settings.EMAIL_HOST_USER],
-    fail_silently=False,
-)       
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[settings.EMAIL_HOST_USER],
+            fail_silently=False,
+        )
+
         return Response({
-            "message" : "Contact saved successfully"
+            "message": "Contact saved successfully"
         })
-        print(serializer.errors)
-        return 
-    return Response(serializer.errors,status=400)
+
+    print(serializer.errors)
+    return Response(serializer.errors, status=400)
